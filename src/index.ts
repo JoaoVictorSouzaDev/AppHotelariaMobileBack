9f99d3a73514d4ab209824f24c602a6f15786c9e
JOAO VICTOR DE SOUZA <joao.vsouza30@senacsp.edu.br>
2025-11-06 15:24:46 -0300

    Feat|Test: criação dos exemplo de acesso ao conteúdo. #1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 import app from "./app";
-import { Request, Response, NextFunction } from "express";
+
 const PORT = 3000;
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-});
-
-// Erro
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).send("Erro na requisição");
-
-});
-
-app.listen(PORT, () => console.log(`Servidor está rodando na PORTA: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is working: ${PORT}`));
